f972d555da86e29a6d7c8083e2ea364969579f98
Eric <equality509@gmail.com>
2023-02-26 19:09:25 -0500

    deploy

--- todoproject/urls.py
+++ todoproject/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from todos.views import TodoViewSet
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Create a Router
 router = routers.DefaultRouter()
@@ -27,4 +29,4 @@ router.register(r'todos', TodoViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path("admin/", admin.site.urls),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
